fc331e626728dbd056f53356d0097dc841cf804a
mikan3rd <hiroki.ota@zenkigen.jp>
2021-10-15 18:45:36 +0900

    chore: delete console

--- src/core/hooks/useTFLite.ts
+++ src/core/hooks/useTFLite.ts
@@ -65,20 +65,20 @@ function useTFLite(segmentationConfig: SegmentationConfig) {
       console.log('Model buffer size:', model.byteLength);
 
       const modelBufferOffset = newSelectedTFLite._getModelBufferMemoryOffset();
-      console.log('Model buffer memory offset:', modelBufferOffset);
-      console.log('Loading model buffer...');
+      // console.log('Model buffer memory offset:', modelBufferOffset);
+      // console.log('Loading model buffer...');
       newSelectedTFLite.HEAPU8.set(new Uint8Array(model), modelBufferOffset);
       console.log('_loadModel result:', newSelectedTFLite._loadModel(model.byteLength));
 
-      console.log('Input memory offset:', newSelectedTFLite._getInputMemoryOffset());
-      console.log('Input height:', newSelectedTFLite._getInputHeight());
-      console.log('Input width:', newSelectedTFLite._getInputWidth());
-      console.log('Input channels:', newSelectedTFLite._getInputChannelCount());
+      // console.log('Input memory offset:', newSelectedTFLite._getInputMemoryOffset());
+      // console.log('Input height:', newSelectedTFLite._getInputHeight());
+      // console.log('Input width:', newSelectedTFLite._getInputWidth());
+      // console.log('Input channels:', newSelectedTFLite._getInputChannelCount());
 
-      console.log('Output memory offset:', newSelectedTFLite._getOutputMemoryOffset());
-      console.log('Output height:', newSelectedTFLite._getOutputHeight());
-      console.log('Output width:', newSelectedTFLite._getOutputWidth());
-      console.log('Output channels:', newSelectedTFLite._getOutputChannelCount());
+      // console.log('Output memory offset:', newSelectedTFLite._getOutputMemoryOffset());
+      // console.log('Output height:', newSelectedTFLite._getOutputHeight());
+      // console.log('Output width:', newSelectedTFLite._getOutputWidth());
+      // console.log('Output channels:', newSelectedTFLite._getOutputChannelCount());
 
       setSelectedTFLite(newSelectedTFLite);
     }

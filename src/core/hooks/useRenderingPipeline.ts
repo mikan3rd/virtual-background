6b4ed36c6eee94e3ce1d049a0f1cb05c623b7aa1
mikan3rd <hiroki.ota@zenkigen.jp>
2021-10-17 21:28:20 +0900

    chore: delete @tensorflow/tfjs

--- src/core/hooks/useRenderingPipeline.ts
+++ src/core/hooks/useRenderingPipeline.ts
@@ -25,8 +25,6 @@ export function useRenderingPipeline(props: Props) {
   const [pipeline, setPipeline] = useState<RenderingPipeline | null>(null);
   const backgroundImageRef = useRef<HTMLImageElement>(document.createElement('img'));
   const canvasRef = useRef<HTMLCanvasElement>(document.createElement('canvas'));
-  // const [fps, setFps] = useState(0);
-  // const [durations, setDurations] = useState<number[]>([]);
 
   const [canvasMediaStreamState, setCanvasMediaStreamState] = useState<MediaStream | null>(null);
   const canvasMediaStreamRef = useRef(canvasMediaStreamState);
@@ -47,12 +45,6 @@ export function useRenderingPipeline(props: Props) {
     // the rendering loop when the framerate is low
     let shouldRender = true;
 
-    // let previousTime = 0;
-    // let beginTime = 0;
-    // let eventCount = 0;
-    // let frameCount = 0;
-    // const frameDurations: number[] = [];
-
     let renderRequestId: number;
 
     canvasRef.current.width = sourceVideoElement.videoWidth;
@@ -67,43 +59,16 @@ export function useRenderingPipeline(props: Props) {
       segmentationBackend,
       canvasRef.current,
       tflite,
-      // addFrameEvent,
     );
 
     async function render() {
       if (!shouldRender) {
         return;
       }
-      // beginFrame();
       await newPipeline.render();
-      // endFrame();
       // eslint-disable-next-line @typescript-eslint/no-misused-promises
       renderRequestId = requestAnimationFrame(render);
     }
-
-    // function beginFrame() {
-    //   beginTime = Date.now();
-    // }
-
-    // function addFrameEvent() {
-    //   const time = Date.now();
-    //   frameDurations[eventCount] = time - beginTime;
-    //   beginTime = time;
-    //   eventCount++;
-    // }
-
-    // function endFrame() {
-    //   const time = Date.now();
-    //   frameDurations[eventCount] = time - beginTime;
-    //   frameCount++;
-    //   if (time >= previousTime + 1000) {
-    //     setFps((frameCount * 1000) / (time - previousTime));
-    //     setDurations(frameDurations);
-    //     previousTime = time;
-    //     frameCount = 0;
-    //   }
-    //   eventCount = 0;
-    // }
 
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     render();
@@ -128,8 +93,6 @@ export function useRenderingPipeline(props: Props) {
 
   return {
     pipeline,
-    // fps,
-    // durations,
     canvasMediaStreamState,
   };
 }
